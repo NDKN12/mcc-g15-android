ddb8d33c886bddc665f59ffccf0d179387b78fac
peterpan121912 <neil.neureither@gmail.com>
2016-10-10 16:24:18 +0300

    Contuinue working on App list

--- app/src/main/java/fi/aalto/openoranges/project1/mcc/MainActivity.java
+++ app/src/main/java/fi/aalto/openoranges/project1/mcc/MainActivity.java
@@ -6,16 +6,11 @@ import android.os.Bundle;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.support.v4.app.ActivityCompat;
 import android.location.Location;
-import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -44,16 +39,14 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     private LocationRequest mLocationRequest;
 
     private static final String LOG_TAG = "MainActivity";
-    public static final MediaType JSON
-            = MediaType.parse("application/json; charset=utf-8");
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
     OkHttpClient client = new OkHttpClient();
     private ApplicationList mAppList = null;
 
-    private double mLatitude;
-    private double mLongtitude;
+
     private TextView mTextView;
-    private TextView mListTextView;
-    private String mListeTest = "void";
+
+    private String mAppsListTest = "void";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,11 +68,11 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         // Toast.makeText(MainActivity.this, mToken, Toast.LENGTH_SHORT).show();
 
         mTextView = (TextView) findViewById(R.id.textView);
-        mListTextView = (TextView) findViewById(R.id.Liste);
+        // mListTextView = (TextView) findViewById(R.id.Liste);
 
 
         mAppList = new ApplicationList("test", "test");
-        mListTextView.setText(mListeTest);
+        //mListTextView.setText(mListeTest);
         mAppList.execute((Void) null);
 
     }
@@ -93,8 +86,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                 .url(url)
                 .addHeader("Authorization", mToken)
                 .build();
-        Response response = client.newCall(request).execute();
-        return response;
+        return client.newCall(request).execute();
     }
 
 
@@ -106,7 +98,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
         private final String mLatitude;
         private final String mLongitude;
-        private String mToken;
 
         ApplicationList(String latitude, String longitude) {
             mLatitude = latitude;
@@ -115,16 +106,18 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
         @Override
         protected Boolean doInBackground(Void... params) {
-            String l = "?lat=12.124124&lng=12.344345";
 
-            // TODO: attempt authentication against a network service.
+            String lat = "12.124124";
+            String lng = "12.344345";
+            String l = "?lat=" + lat + "&lng=" + lng;
+
             try {
                 // Simulate network access.
                 Response response = getList("https://mccg15.herokuapp.com/application" + l);
                 int code = response.code();
                 if (code == 200) {
-                    mListeTest = response.body().string().toString();
-                    Toast.makeText(MainActivity.this, mListeTest, Toast.LENGTH_LONG).show();
+                    mAppsListTest = response.body().string().toString();
+                    Toast.makeText(MainActivity.this, mAppsListTest, Toast.LENGTH_LONG).show();
                 } else {
                     Toast.makeText(MainActivity.this, "Code: 401", Toast.LENGTH_LONG).show();
                     return false;
@@ -145,34 +138,30 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
             ArrayList<JSONObject> arrays = null;
             if (success) {
 
-                mListeTest = "{'apps': " + mListeTest + "}";
+                mAppsListTest = "{'apps': " + mAppsListTest + "}";
                 try {
-                    JSONObject myjson = new JSONObject(mListeTest);
+                    JSONObject myjson = new JSONObject(mAppsListTest);
                     JSONArray the_json_array = myjson.getJSONArray("apps");
                     int size = the_json_array.length();
-                     arrays = new ArrayList<JSONObject>();
+                    arrays = new ArrayList<JSONObject>();
                     for (int i = 0; i < size; i++) {
                         JSONObject another_json_object = the_json_array.getJSONObject(i);
-                        //Blah blah blah...
+
                         arrays.add(another_json_object);
                     }
 
                     JSONObject[] jsons = new JSONObject[arrays.size()];
                     arrays.toArray(jsons);
 
-
-
-                    //  JSONObject   myjson = new JSONObject(mListeTest);
-                    //  JSONArray nameArray = myjson.names();
-                    //  JSONArray valArray = myjson.toJSONArray(nameArray);
-                    //  for (int i = 0; i < valArray.length(); i++) {
-                    //    String p = nameArray.getString(i) + "," + valArray.getString(i);
-                    //     Log.i("p", p);
-                    // }
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-                Toast.makeText(MainActivity.this, arrays.get(0).keys().toString(), Toast.LENGTH_LONG).show();
+                assert arrays != null;
+                try {
+                    Toast.makeText(MainActivity.this, arrays.get(0).getString("icon_url").toString(), Toast.LENGTH_LONG).show();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             } else {
                 Toast.makeText(MainActivity.this, "ApplicationList Failure!", Toast.LENGTH_SHORT).show();
             }
@@ -204,7 +193,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     public void onConnected(@Nullable Bundle bundle) {
 
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
             //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
@@ -236,8 +224,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     public void onLocationChanged(Location location) {
         Log.i(LOG_TAG, location.toString());
         mTextView.setText(location.toString());
-        mLatitude = location.getLatitude();
-        mLongtitude = location.getLongitude();
     }
 
 }
